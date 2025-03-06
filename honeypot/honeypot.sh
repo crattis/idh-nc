2274ff6ab2a5a52e8dc64dfb096ddee36428296f
Chris J <crattis@users.noreply.github.com>
2025-03-06 18:41:58 -0500

    Update honeypot.sh
    Changed echo to printf.
    added examples of how to place port numbers in the array section

--- honeypot/honeypot.sh
+++ honeypot/honeypot.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Set date to be added at start of file when script is ran.
-NOW='$(date +"%Y-%m-%d %H:%M:%S:z")'
+NOW=$(date +"%Y-%m-%d %H:%M:%S:%z")
 
 # Port Array needs to be set up for the local environment. Get a list of workstations with open port, match highsest counts.
-port_array=()
+port_array=(<port number> <port number>)
 
 # For loop to run NC and put in the time stamps when script startd and on each event.
 for port in ${port_array[@]}
 do
-echo "$NOW listner started" >> /var/log/honeypot/$port.log 2>&1 &
-ncat -klvnp $port 2>&1 | ts '%Y-%m-%d %H:%M:%S%z | tee -a /var/log/honeypot/$port.log &
+printf  "$NOW listner started \n" >> /var/log/honeypot/$port.log 2>&1 &
+ncat -klvnp $port 2>&1 | ts '%Y-%m-%d %H:%M:%S%z' | tee -a /var/log/honeypot/$port.log &
 done

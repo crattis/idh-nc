af02b52cdf4c7664fe359a025a227e63139680a6
Chris J <crattis@users.noreply.github.com>
2025-03-07 14:02:11 -0500

    Update honeypot.sh

--- honeypot/honeypot.sh
+++ honeypot/honeypot.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Set date to be added at start of file when script is ran.
+# Set the timestamp to be added to the file when the script is started.
 NOW=$(date +"%Y-%m-%d %H:%M:%S:%z")
 
-# Port Array needs to be set up for the local environment. Get a list of workstations with open port, match highsest counts.
+# Port Array needs to be set up for the local environment. Get a list of workstations with open port, match highest counts.
 port_array=(<port number> <port number>)
 
-# For loop to run NC and put in the time stamps when script startd and on each event.
+# For loop to run NC and put in the time stamps when script started and on each event.
 for port in ${port_array[@]}
 do
-printf  "$NOW listner started \n" >> /var/log/honeypot/$port.log 2>&1 &
+printf  "$NOW listener started \n" >> /var/log/honeypot/$port.log 2>&1 &
 ncat -klvnp $port 2>&1 | ts '%Y-%m-%d %H:%M:%S%z' | tee -a /var/log/honeypot/$port.log &
 done
